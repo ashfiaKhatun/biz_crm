dd7eb34ee51d06de834e2da80cfe7368f407f545
Atick Eashrak Shuvo <atick@Aticks-MacBook-Pro.local>
2024-05-23 18:00:19 +0600

    Layout fixed

--- resources/views/template/auth/register_manager.blade.php
+++ resources/views/template/auth/register_manager.blade.php
@@ -11,7 +11,7 @@
 
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-    <link href="template/css/style.css" rel="stylesheet">
+    <link href="../template/css/style.css" rel="stylesheet">
 
 </head>
 
@@ -89,11 +89,11 @@
     <!--**********************************
         Scripts
     ***********************************-->
-    <script src="template/plugins/common/common.min.js"></script>
-    <script src="template/js/custom.min.js"></script>
-    <script src="template/js/settings.js"></script>
-    <script src="template/js/gleek.js"></script>
-    <script src="template/js/styleSwitcher.js"></script>
+    <script src="../template/plugins/common/common.min.js"></script>
+    <script src="../template/js/custom.min.js"></script>
+    <script src="../template/js/settings.js"></script>
+    <script src="../template/js/gleek.js"></script>
+    <script src="../template/js/styleSwitcher.js"></script>
 </body>
 
 </html>
