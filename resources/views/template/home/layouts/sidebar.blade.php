4901c83b6eb575bb238449fac8f5a689760db4b4
Atick Eashrak Shuvo <atick@Aticks-MacBook-Pro.local>
2024-05-29 12:14:56 +0600

    updated sidebar

--- resources/views/template/home/layouts/sidebar.blade.php
+++ resources/views/template/home/layouts/sidebar.blade.php
@@ -2,13 +2,10 @@
     <div class="nk-nav-scroll">
         <ul class="metismenu" id="menu">
             <li>
-                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                <a  href="{{ route('dashboard') }}" aria-expanded="false">
                     <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
                 </a>
-                <ul aria-expanded="false">
-                    <li><a href="{{ route('dashboard') }}">Home 1</a></li>
-                    <!-- <li><a href="./index-2.html">Home 2</a></li> -->
-                </ul>
+                
             </li>
 
             <li class="mega-menu mega-menu-sm">
