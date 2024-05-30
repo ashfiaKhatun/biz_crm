5df42b8cf047cff9e1b6fc7dc61f4c34bd7ed448
ashfiaKhatun <ashfia1204@gmail.com>
2024-05-30 08:46:34 +0200

    update refill form created

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,15 +7,5 @@ use Illuminate\Validation\ValidationException; // Import the ValidationException
 
 class HomeController extends Controller
 {
-    public function ad_account_application()
-    {
-        return view('template.home.ad_account.ad_account_application');
-    }
     
-    
-    
-    public function add_agency()
-    {
-        return view('template.home.agencies.add_agency');
-    }
 }
