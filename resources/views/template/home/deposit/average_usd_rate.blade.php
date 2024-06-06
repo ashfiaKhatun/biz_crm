a85270ce8b2fca0135ffd829d5604e6ec818cb3e
ashfiaKhatun <ashfia1204@gmail.com>
2024-06-06 14:43:04 +0200

    design modified

--- resources/views/template/home/deposit/average_usd_rate.blade.php
+++ resources/views/template/home/deposit/average_usd_rate.blade.php
@@ -9,28 +9,34 @@
 @include('template.home.layouts.navbar')
 @include('template.home.layouts.sidebar')
 
-<div class="content-body">
-    <div class="container my-5">
-        <h2>Average USD Rate per Month</h2>
+<div class="content-body p-4">
+    <div class="card">
+        <div class="card-body">
+            <h2 class="card-title">Average USD Rate per Month</h2>
+    
+            <div class="table-responsive">
+                <table class="table table-bordered table-striped verticle-middle">
+                    <thead>
+                        <tr>
+                            <th>Year</th>
+                            <th>Month</th>
+                            <th>Average USD Rate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($averageRates as $rate)
+                            <tr>
+                                <td>{{ $rate->year }}</td>
+                                <td>{{ \Carbon\Carbon::create()->month($rate->month)->format('F') }}</td>
+                                <td>{{ $rate->average_rate }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+    
+            </div>
 
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th>Year</th>
-                    <th>Month</th>
-                    <th>Average USD Rate</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($averageRates as $rate)
-                    <tr>
-                        <td>{{ $rate->year }}</td>
-                        <td>{{ \Carbon\Carbon::create()->month($rate->month)->format('F') }}</td>
-                        <td>{{ $rate->average_rate }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+        </div>
     </div>
 </div>
 
--- resources/views/template/home/deposit/index.blade.php
+++ resources/views/template/home/deposit/index.blade.php
@@ -10,99 +10,108 @@
     @include('template.home.layouts.navbar')
     @include('template.home.layouts.sidebar')
 
-    <div class="content-body p-4">
-        <div class="card">
-            <div class="card-body">
-                <h2 class="card-title">All Deposits</h2>
+    <div class="content-body">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <h2 class="card-title">All Deposits</h2>
 
-                @if (session('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
+                            @if (session('success'))
+                            <div class="alert alert-success">
+                                {{ session('success') }}
+                            </div>
+                            @endif
+
+                            <div class="table-responsive">
+
+                                <table class="table table-bordered table-striped verticle-middle">
+                                    <thead>
+                                        <tr>
+
+                                            <th>Name</th>
+                                            <th>Amount (USD)</th>
+                                            <th>Rate (BDT)</th>
+
+                                            <th>Status</th>
+                                            <th>Actions</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($deposits as $deposit)
+                                        <tr>
+
+                                            <td>{{ $deposit->name }}</td>
+                                            <td>{{ $deposit->amount_usd }}</td>
+                                            <td>{{ $deposit->rate_bdt }}</td>
+
+                                            <td>
+                                                <form action="{{ route('deposit.updateStatus', $deposit->id) }}" method="post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <select name="status" class="form-control" onchange="this.form.submit()">
+                                                        <option value="pending" {{ $deposit->status == 'pending' ? 'selected' : '' }}>Pending
+                                                        </option>
+                                                        <option value="received" {{ $deposit->status == 'received' ? 'selected' : '' }}>Received
+                                                        </option>
+                                                        <option value="canceled" {{ $deposit->status == 'canceled' ? 'selected' : '' }}>Canceled
+                                                        </option>
+                                                    </select>
+                                                </form>
+                                            </td>
+                                            <td>
+                                                <span>
+                                                    <a href="{{ route('deposit.show', $deposit->id) }}" data-toggle="tooltip" data-placement="top" title="View">
+                                                        <i class="fa fa-eye color-muted m-r-5"></i>
+                                                    </a>
+
+                                                    <a href="{{ route('deposit.edit', $deposit->id) }}" data-toggle="tooltip" data-placement="top" title="Edit">
+                                                        <i class="fa fa-pencil color-muted m-r-5 ml-3"></i>
+                                                    </a>
+
+                                                    <form action="{{ route('deposit.destroy', $deposit->id) }}" method="POST" style="display:inline-block;">
+                                                        @csrf
+                                                        @method('DELETE')
+                                                        <button type="submit" class="border-0 bg-transparent ml-3" onclick="return confirm('Are you sure you want to delete this deposit?')" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-close color-danger"></i></button>
+                                                    </form>
+                                                </span>
+
+                                            </td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+
                     </div>
-                @endif
+                </div>
 
-                <div class="table-responsive">
-                    <div class="mb-3">
-                        @foreach ($averageRates as $rate)
-                            <strong>{{ \Carbon\Carbon::create()->month($rate->month)->format('F') }}
-                                ({{ $rate->year }}) avarage USD Rate {{ $rate->average_rate }}</strong>
-                        @endforeach
-                        <table class="table table-bordered">
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                        <h5 class="mb-3">Last three months average USD rate</h5>
+                            @foreach ($averageRates as $rate)
+                            <div class="row">
+                                <b class="col-5">{{ \Carbon\Carbon::create()->month($rate->month)->format('F') }}
+                                    ({{ $rate->year }})</b>
+                                <p class="col-7"> {{ $rate->average_rate }}</p>
 
-
-                    </div>
-                    <table class="table table-bordered table-striped verticle-middle">
-                        <thead>
-                            <tr>
-
-                                <th>Name</th>
-                                <th>Amount (USD)</th>
-                                <th>Rate (BDT)</th>
-
-                                <th>Status</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($deposits as $deposit)
-                                <tr>
-
-                                    <td>{{ $deposit->name }}</td>
-                                    <td>{{ $deposit->amount_usd }}</td>
-                                    <td>{{ $deposit->rate_bdt }}</td>
-
-                                    <td>
-                                        <form action="{{ route('deposit.updateStatus', $deposit->id) }}" method="post">
-                                            @csrf
-                                            @method('PATCH')
-                                            <select name="status" class="form-control" onchange="this.form.submit()">
-                                                <option value="pending"
-                                                    {{ $deposit->status == 'pending' ? 'selected' : '' }}>Pending
-                                                </option>
-                                                <option value="received"
-                                                    {{ $deposit->status == 'received' ? 'selected' : '' }}>Received
-                                                </option>
-                                                <option value="canceled"
-                                                    {{ $deposit->status == 'canceled' ? 'selected' : '' }}>Canceled
-                                                </option>
-                                            </select>
-                                        </form>
-                                    </td>
-                                    <td>
-                                        <span>
-                                            <a href="{{ route('deposit.show', $deposit->id) }}" data-toggle="tooltip"
-                                                data-placement="top" title="View">
-                                                <i class="fa fa-eye color-muted m-r-5"></i>
-                                            </a>
-
-                                            <a href="{{ route('deposit.edit', $deposit->id) }}" data-toggle="tooltip"
-                                                data-placement="top" title="Edit">
-                                                <i class="fa fa-pencil color-muted m-r-5 ml-3"></i>
-                                            </a>
-
-                                            <form action="{{ route('deposit.destroy', $deposit->id) }}" method="POST"
-                                                style="display:inline-block;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="border-0 bg-transparent ml-3"
-                                                    onclick="return confirm('Are you sure you want to delete this deposit?')"
-                                                    data-toggle="tooltip" data-placement="top" title="Delete"><i
-                                                        class="fa fa-close color-danger"></i></button>
-                                            </form>
-                                        </span>
-
-                                    </td>
-                                </tr>
+                            </div>
                             @endforeach
-                        </tbody>
-                    </table>
+
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
 
         </div>
     </div>
-</div>
+    </div>
 
     @include('template.home.layouts.footer')
     @include('template.home.layouts.scripts')
