b8cbfc88c68ab7dc17beefa5fd8c85be7609b7af
ashfiaKhatun <ashfia1204@gmail.com>
2024-06-06 08:21:08 +0200

    client details added

--- resources/views/template/home/ad_account/show.blade.php
+++ resources/views/template/home/ad_account/show.blade.php
@@ -1,113 +1,177 @@
 <!DOCTYPE html>
 <html lang="en">
 
-@include('template.home.layouts.head')
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    
+    <!-- theme meta -->
+    <meta name="theme-name" content="quixlab" />
+  
+    <title>BizMappers</title>
+    <!-- Favicon icon -->
+    <link rel="icon" type="../../template/image/png" sizes="16x16" href="../../template/images/favicon.png">
+    <!-- Pignose Calender -->
+    <link href="../../template/plugins/pg-calendar/css/pignose.calendar.min.css" rel="stylesheet">
+    <!-- Chartist -->
+    <link rel="stylesheet" href="../../template/plugins/chartist/css/chartist.min.css">
+    <link rel="stylesheet" href="../../template/plugins/chartist-plugin-tooltips/css/chartist-plugin-tooltip.css">
+    <!-- Custom Stylesheet -->
+    <link href="../../template/css/style.css" rel="stylesheet">
+    <link href="../../template/css/style.css" rel="stylesheet">
+
+    <style>
+        .text-black{
+            color: black;
+        }
+        .font-sm{
+            font-size: 13px;
+        }
+    </style>
+
+
+    <script src="https://kit.fontawesome.com/eabba84056.js" crossorigin="anonymous"></script>
+
+</head>
 
 <body>
 
     @include('template.home.layouts.navbar')
     @include('template.home.layouts.sidebar')
 
-    <div class="content-body p-4">
-        <div class="container">
+    <div class="content-body">
+        <div class="container-fluid">
 
-            <div class="card">
-                <div class="card-body">
-                    <div class="d-flex">
-                        <h4 class="card-title mb-5">Details of {{ $adAccount->ad_acc_name }}</h4>
-                        <a href="{{ route('ad-account.edit', $adAccount->id) }}" data-toggle="tooltip" data-placement="top" title="Edit">
-                            <i class="fa fa-pencil color-muted m-r-5 ml-3"></i>
-                        </a>
+            <div class="row text-black">
+                <div class="col-8">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="d-flex align-items-center mb-5">
+                                <h4 class="card-title mr-4 mt-2">Details of {{ $adAccount->ad_acc_name }}</h4>
+                                <a href="{{ route('ad-account.edit', $adAccount->id) }}">
+                                    <button class="btn btn-secondary">Edit Info<i class="fa fa-pencil color-muted m-r-5 ml-3"></i></button>
+                                </a>
 
-                    </div>
+                            </div>
 
-                    <div class="row">
-                        <b class="col-3 text-black">Client Name:</b>
-                        <p class="col-9 fs-4">{{ $adAccount->client->name }}</p>
-                    </div>
-                    <div class="row">
-                        <strong class="col-3">Ad Account Name:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->ad_acc_name }}</p>
-                    </div>
-                    <div class="row">
-                        <strong class="col-3">Business Manager ID:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->bm_id }}</p>
-                    </div>
+                            <div class="row">
+                                <b class="col-4">Ad Account Name:</b>
+                                <p class="col-8 ">{{ $adAccount->ad_acc_name }}</p>
+                            </div>
+                            <div class="row">
+                                <b class="col-4">Business Manager ID:</b>
+                                <p class="col-8 ">{{ $adAccount->bm_id }}</p>
+                            </div>
 
-                    @if(isset($adAccount->fb_link1) && $adAccount->fb_link1 !== '')
-                    <div class="row">
-                        <strong class="col-3">Facebook Link 1:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->fb_link1 }}</p>
-                    </div>
-                    @endif
+                            @if(isset($adAccount->fb_link1) && $adAccount->fb_link1 !== '')
+                            <div class="row">
+                                <b class="col-4">Facebook Link 1:</b>
+                                <p class="col-8 ">{{ $adAccount->fb_link1 }}</p>
+                            </div>
+                            @endif
 
-                    @if(isset($adAccount->fb_link2) && $adAccount->fb_link2 !== '')
-                    <div class="row">
-                        <strong class="col-3">Facebook Link 2:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->fb_link2 }}</p>
-                    </div>
-                    @endif
+                            @if(isset($adAccount->fb_link2) && $adAccount->fb_link2 !== '')
+                            <div class="row">
+                                <b class="col-4">Facebook Link 2:</b>
+                                <p class="col-8 ">{{ $adAccount->fb_link2 }}</p>
+                            </div>
+                            @endif
 
-                    @if(isset($adAccount->fb_link3) && $adAccount->fb_link3 !== '')
-                    <div class="row">
-                        <strong class="col-3">Facebook Link 3:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->fb_link3 }}</p>
-                    </div>
-                    @endif
+                            @if(isset($adAccount->fb_link3) && $adAccount->fb_link3 !== '')
+                            <div class="row">
+                                <b class="col-4">Facebook Link 3:</b>
+                                <p class="col-8 ">{{ $adAccount->fb_link3 }}</p>
+                            </div>
+                            @endif
 
-                    @if(isset($adAccount->fb_link4) && $adAccount->fb_link4 !== '')
-                    <div class="row">
-                        <strong class="col-3">Facebook Link 4:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->fb_link4 }}</p>
-                    </div>
-                    @endif
+                            @if(isset($adAccount->fb_link4) && $adAccount->fb_link4 !== '')
+                            <div class="row">
+                                <b class="col-4">Facebook Link 4:</b>
+                                <p class="col-8 ">{{ $adAccount->fb_link4 }}</p>
+                            </div>
+                            @endif
 
-                    @if(isset($adAccount->fb_link5) && $adAccount->fb_link5 !== '')
-                    <div class="row">
-                        <strong class="col-3">Facebook Link 5:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->fb_link5 }}</p>
-                    </div>
-                    @endif
-                    
-                    @if(isset($adAccount->domain1) && $adAccount->domain1 !== '')
-                    <div class="row">
-                        <strong class="col-3">Domain 1:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->domain1 }}</p>
-                    </div>
-                    @endif
+                            @if(isset($adAccount->fb_link5) && $adAccount->fb_link5 !== '')
+                            <div class="row">
+                                <b class="col-4">Facebook Link 5:</b>
+                                <p class="col-8 ">{{ $adAccount->fb_link5 }}</p>
+                            </div>
+                            @endif
 
-                    @if(isset($adAccount->domain2) && $adAccount->domain2 !== '')
-                    <div class="row">
-                        <strong class="col-3">Domain 2:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->domain2 }}</p>
-                    </div>
-                    @endif
+                            @if(isset($adAccount->domain1) && $adAccount->domain1 !== '')
+                            <div class="row">
+                                <b class="col-4">Domain 1:</b>
+                                <p class="col-8 ">{{ $adAccount->domain1 }}</p>
+                            </div>
+                            @endif
 
-                    @if(isset($adAccount->domain3) && $adAccount->domain3 !== '')
-                    <div class="row">
-                        <strong class="col-3">Domain 3:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->domain3 }}</p>
-                    </div>
-                    @endif
-                  
-                    <div class="row">
-                        <strong class="col-3">Agency:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->agency->agency_name }}</p>
-                    </div>
-                    <div class="row">
-                        <strong class="col-3">Ad Account Type:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->ad_acc_type }}</p>
-                    </div>
-                    <div class="row">
-                        <strong class="col-3">Dollar Rate:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->dollar_rate }}</p>
-                    </div>
-                    <div class="row">
-                        <strong class="col-3">Status:</strong>
-                        <p class="col-9 fs-4">{{ $adAccount->status }}</p>
+                            @if(isset($adAccount->domain2) && $adAccount->domain2 !== '')
+                            <div class="row">
+                                <b class="col-4">Domain 2:</b>
+                                <p class="col-8 ">{{ $adAccount->domain2 }}</p>
+                            </div>
+                            @endif
+
+                            @if(isset($adAccount->domain3) && $adAccount->domain3 !== '')
+                            <div class="row">
+                                <b class="col-4">Domain 3:</b>
+                                <p class="col-8 ">{{ $adAccount->domain3 }}</p>
+                            </div>
+                            @endif
+
+                            <div class="row">
+                                <b class="col-4">Agency:</b>
+                                <p class="col-8 ">{{ $adAccount->agency->agency_name }}</p>
+                            </div>
+                            <div class="row">
+                                <b class="col-4">Ad Account Type:</b>
+                                <p class="col-8 ">{{ $adAccount->ad_acc_type }}</p>
+                            </div>
+                            <div class="row">
+                                <b class="col-4">Dollar Rate:</b>
+                                <p class="col-8 ">{{ $adAccount->dollar_rate }}</p>
+                            </div>
+                            <div class="row">
+                                <b class="col-4">Status:</b>
+                                <p class="col-8 ">{{ $adAccount->status }}</p>
+                            </div>
+
+                        </div>
                     </div>
 
                 </div>
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body font-sm">
+
+                            <h4 class="card-title mb-5">Clients' Information</h4>
+                            <div class="row">
+                                <b class="col-5">Client Name:</b>
+                                <p class="col-7">{{ $adAccount->client->name }}</p>
+                            </div>
+                            <div class="row">
+                                <b class="col-5">Phone Number:</b>
+                                <p class="col-7">{{ $adAccount->client->phone }}</p>
+                            </div>
+                            <div class="row">
+                                <b class="col-5">Email:</b>
+                                <p class="col-7">{{ $adAccount->client->email }}</p>
+                            </div>
+                            <div class="row">
+                                <b class="col-5">Business Type:</b>
+                                <p class="col-7">{{ $adAccount->client->business_type }}</p>
+                            </div>
+                            <div class="row">
+                                <b class="col-5">Business Name:</b>
+                                <p class="col-7">{{ $adAccount->client->business_name }}</p>
+                            </div>
+
+                        </div>
+                    </div>
+
+                </div>
+
             </div>
 
             <a href="{{ route('ad-account.index') }}" class="btn btn-secondary mt-3">Back</a>
