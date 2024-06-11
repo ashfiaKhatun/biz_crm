f35f008435a29cf4ddfecea2f9323dee4c7d14f9
ashfiaKhatun <ashfia1204@gmail.com>
2024-06-11 14:32:45 +0600

    table scrollable and send to agency button

--- resources/views/template/home/refill_application/index.blade.php
+++ resources/views/template/home/refill_application/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     @include('template.home.layouts.head')
     <style>
-        .font-sm{
+        .font-sm {
             font-size: 12px;
         }
     </style>
@@ -18,7 +18,7 @@
     <div class="content-body p-4">
         <div class="card">
             <div class="card-body">
-            <div class="d-flex justify-content-between align-items-center mb-5">
+                <div class="d-flex justify-content-between align-items-center mb-5">
                     <h4 class="card-title mr-4 mt-2">Refill Applications</h4>
                     <a href="{{ route('refill-application') }}">
                         <button class="btn btn-secondary">New Refill<i class="fa fa-plus color-muted m-r-5 ml-2"></i></button>
@@ -26,9 +26,9 @@
                 </div>
 
                 @if (session('success'))
-                    <div class="alert alert-success">
-                        {{ session('success') }}
-                    </div>
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
                 @endif
 
                 <div class="table-responsive text-nowrap">
@@ -41,6 +41,7 @@
                                 <th>Amount (Dollar)</th>
                                 <th>Amount (Taka)</th>
                                 <th>Method</th>
+                                <th></th>
 
                                 <th>Status</th>
                                 <th>Actions</th>
@@ -48,67 +49,64 @@
                         </thead>
                         <tbody>
                             @foreach ($refills as $refill)
-                                <tr>
-                                    <td>{{ $refill->created_at->format('j F Y || g:i a') }}</td>
-                                    <td>{{ $refill->adAccount->ad_acc_name }}</td>
-                                    <td>{{ $refill->adAccount->dollar_rate }}</td>
-                                    <td>{{ $refill->amount_dollar }}</td>
-                                    <td>{{ $refill->amount_taka }}</td>
+                            <tr>
+                                <td>{{ $refill->created_at->format('j F Y || g:i a') }}</td>
+                                <td>{{ $refill->adAccount->ad_acc_name }}</td>
+                                <td>{{ $refill->adAccount->dollar_rate }}</td>
+                                <td>{{ $refill->amount_dollar }}</td>
+                                <td>{{ $refill->amount_taka }}</td>
 
-                                    <td>{{ $refill->payment_method }}</td>
+                                <td>{{ $refill->payment_method }}</td>
 
-                                    <td>
-                                        <form action="{{ route('refills.updateStatus', $refill->id) }}" method="post">
+                                <td class="text-center">
+                                    @if ($refill->sent_to_agency == 0)
+                                    <form action="{{ route('refill.sendToAgency', $refill->id) }}" method="post" style="display:inline-block;">
+                                        @csrf
+                                        <button type="submit" class="border-0 bg-transparent" onclick="return confirm('Are you sure you want to send this refill application to the agency?')">
+                                            <span class="badge badge-primary">Send to Agency</span>
+                                        </button>
+                                    </form>
+                                    @else
+                                    <span class="badge badge-warning" id="buttonText">Sent</span>
+                                    @endif
+                                </td>
+
+                                <td>
+                                    <form action="{{ route('refills.updateStatus', $refill->id) }}" method="post">
+                                        @csrf
+                                        @method('PATCH')
+                                        <select name="status" class="form-control"  style="width: 100px;" onchange="this.form.submit()">
+                                            <option value="pending" {{ $refill->status == 'pending' ? 'selected' : '' }}>Pending
+                                            </option>
+                                            <option value="approved" {{ $refill->status == 'approved' ? 'selected' : '' }}>Approved
+                                            </option>
+                                            <option value="rejected" {{ $refill->status == 'rejected' ? 'selected' : '' }}>Rejected
+                                            </option>
+                                        </select>
+                                    </form>
+                                </td>
+
+                                <td>
+                                    <span>
+                                        <a href="{{ route('refills.show', $refill->id) }}" data-toggle="tooltip" data-placement="top" title="View">
+                                            <i class="fa fa-eye color-muted m-r-5"></i>
+                                        </a>
+
+                                        <a href="{{ route('refills.edit', $refill->id) }}" data-toggle="tooltip" data-placement="top" title="Edit">
+                                            <i class="fa fa-pencil color-muted m-r-5 ml-3"></i>
+                                        </a>
+
+                                        <form action="{{ route('refills.destroy', $refill->id) }}" method="POST" style="display:inline-block;">
                                             @csrf
-                                            @method('PATCH')
-                                            <select name="status" class="form-control" onchange="this.form.submit()">
-                                                <option value="pending"
-                                                    {{ $refill->status == 'pending' ? 'selected' : '' }}>Pending
-                                                </option>
-                                                <option value="approved"
-                                                    {{ $refill->status == 'approved' ? 'selected' : '' }}>Approved
-                                                </option>
-                                                <option value="rejected"
-                                                    {{ $refill->status == 'rejected' ? 'selected' : '' }}>Rejected
-                                                </option>
-                                            </select>
+                                            @method('DELETE')
+                                            <button type="submit" class="border-0 bg-transparent ml-3" onclick="return confirm('Are you sure you want to delete this refill application?')" data-toggle="tooltip" data-placement="top" title="Delete"><i class="fa fa-close color-danger"></i></button>
                                         </form>
-                                    </td>
 
-                                    <td>
-                                        <span>
-                                            <a href="{{ route('refills.show', $refill->id) }}" data-toggle="tooltip"
-                                                data-placement="top" title="View">
-                                                <i class="fa fa-eye color-muted m-r-5"></i>
-                                            </a>
 
-                                            <a href="{{ route('refills.edit', $refill->id) }}" data-toggle="tooltip"
-                                                data-placement="top" title="Edit">
-                                                <i class="fa fa-pencil color-muted m-r-5 ml-3"></i>
-                                            </a>
+                                    </span>
+                                </td>
 
-                                            <form action="{{ route('refills.destroy', $refill->id) }}" method="POST"
-                                                style="display:inline-block;">
-                                                @csrf
-                                                @method('DELETE')
-                                                <button type="submit" class="border-0 bg-transparent ml-3"
-                                                    onclick="return confirm('Are you sure you want to delete this refill application?')"
-                                                    data-toggle="tooltip" data-placement="top" title="Delete"><i
-                                                        class="fa fa-close color-danger"></i></button>
-                                            </form>
-
-                                            @if ($refill->sent_to_agency == 0)
-                                            <form action="{{ route('refill.sendToAgency', $refill->id) }}" method="post" style="display:inline-block;">
-                                                @csrf
-                                                <button type="submit" class="border-0 bg-transparent ml-1" onclick="return confirm('Are you sure you want to send this refill application to the agency?')" data-toggle="tooltip" data-placement="top" title="Send to Agency">
-                                                    <i class="fa fa-send color-primary"></i>
-                                                </button>
-                                            </form>
-                                            @endif
-                                        </span>
-                                    </td>
-
-                                </tr>
+                            </tr>
                             @endforeach
                         </tbody>
                     </table>
