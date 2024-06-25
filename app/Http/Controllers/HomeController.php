f26eb1ad1055a02587704a7c0008441f37bb03bb
Atick Eashrak Shuvo <atick@Aticks-MacBook-Pro.local>
2024-06-25 17:07:50 +0600

    dashbord

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,7 +5,11 @@ namespace App\Http\Controllers;
 use App\Models\AdAccount;
 use App\Models\Refill;
 use App\Models\SystemNotification;
+use App\Models\Deposit;
+use App\Models\User;
+use App\Models\Settings;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException; // Import the ValidationException class
 
@@ -46,7 +50,31 @@ class HomeController extends Controller
             ->get();
 
 
-        return view('template.home.index', compact('allApplication', 'pendingApplication', 'allAdAccount', 'thisMonthRefill', 'pendingRefillCount', 'pendingRefillAmount', 'lastSevenDaysRefill', 'refills', 'adAccounts'));
+
+        $startOfMonth = Carbon::now()->startOfMonth();
+        $endOfMonth = Carbon::now()->endOfMonth();
+
+
+        $totalDeposit = Deposit::whereBetween('created_at', [$startOfMonth, $endOfMonth])
+            ->sum('amount_usd');
+
+
+
+        $result = Deposit::select(
+            DB::raw('SUM(amount_bdt) as total_bdt'),
+            DB::raw('SUM(amount_usd) as total_usd'),
+            DB::raw('IF(SUM(amount_usd) = 0, 0, SUM(amount_bdt) / SUM(amount_usd)) as average_rate')
+        )
+            ->whereBetween('created_at', [$startOfMonth, $endOfMonth])
+            ->first();
+
+        $averageRate = $result->average_rate;
+
+
+        $customers = User::where('role', 'customer')->get();
+        $paymentMethods = Settings::where('setting_name', 'Refill Payment Method')->get();
+        $vendors = Settings::where('setting_name', 'Vendor')->get();
+        return view('template.home.index', compact('allApplication', 'pendingApplication', 'allAdAccount', 'thisMonthRefill', 'pendingRefillCount', 'pendingRefillAmount', 'lastSevenDaysRefill', 'refills', 'adAccounts', 'totalDeposit','averageRate','customers','paymentMethods','vendors'));
     }
 
     public function index()
--- resources/views/template/home/index.blade.php
+++ resources/views/template/home/index.blade.php
@@ -20,7 +20,8 @@
     <div id="preloader">
         <div class="loader">
             <svg class="circular" viewBox="25 25 50 50">
-                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3" stroke-miterlimit="10" />
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="3"
+                    stroke-miterlimit="10" />
             </svg>
         </div>
     </div>
@@ -71,24 +72,28 @@
                             <div class="p-4">
                                 <h5 class="text-white">Current Month Refill: <span>{{ $thisMonthRefill }}</span></h5>
                                 <h5 class="text-white">Last 7 Days Refill: <span>{{ $lastSevenDaysRefill }}</span></h5>
-                                <h5 class="text-white">Refill Request: <span>{{ $pendingRefillAmount }} ({{$pendingRefillCount}})</span></h5>
+                                <h5 class="text-white">Refill Request: <span>{{ $pendingRefillAmount }}
+                                        ({{ $pendingRefillCount }})</span></h5>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="d-flex align-items-center custom-card gradient-3">
                             <div class="p-4">
-                                <h5 class="text-white">Current Month Deposit: <span></span></h5>
-                                <h5 class="text-white">Average Rate: <span></span></h5>
+                                <h5 class="text-white">Current Month Deposit: <span>${{ $totalDeposit }}</span></h5>
+                                <h5 class="text-white">Average Rate: <span>৳{{ $averageRate }}</span></h5>
                                 <h5 class="text-white">Balance: <span></span></h5>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="btn-group-vertical w-100">
-                            <a class="btn btn-primary text-white mb-1 py-3 rounded">New Refill</a>
-                            <a class="btn btn-secondary text-white mb-1 py-3 rounded">New Account</a>
-                            <a class="btn btn-success text-white py-3 rounded">Add Deposit</a>
+                            <a href="#" class="btn btn-primary text-white mb-1 py-3 rounded" data-toggle="modal"
+                                data-target="#refillModal">New Refill</a>
+                            <a href="{{ route('ad-account-application') }}"
+                                class="btn btn-secondary text-white mb-1 py-3 rounded">New Application</a>
+                            <a href="#" class="btn btn-success text-white py-3 rounded" data-toggle="modal"
+                                data-target="#depositModal">Add Deposit</a>
 
                         </div>
                     </div>
@@ -118,30 +123,31 @@
                                         </thead>
                                         <tbody>
                                             @foreach ($refills as $refill)
-                                            <tr>
-                                                <td>{{ $refill->created_at->format('j F Y') }}</td>
-                                                <td>
-                                                    <span>{{ $refill->adAccount->ad_acc_name }}</span><br>
-                                                    <span class="font-sm mt-1">{{ $refill->adAccount->ad_acc_id }}</span>
-                                                </td>
-                                                <td>{{ $refill->amount_dollar }}</td>
-                                                <td>{{ $refill->payment_method }}</td>
-                                                <td>
-                                                    @if ($refill->status == 'pending')
-                                                    <span class="badge custom-badge-info">Pending</span>
-                                                    @endif
+                                                <tr>
+                                                    <td>{{ $refill->created_at->format('j F Y') }}</td>
+                                                    <td>
+                                                        <span>{{ $refill->adAccount->ad_acc_name }}</span><br>
+                                                        <span
+                                                            class="font-sm mt-1">{{ $refill->adAccount->ad_acc_id }}</span>
+                                                    </td>
+                                                    <td>{{ $refill->amount_dollar }}</td>
+                                                    <td>{{ $refill->payment_method }}</td>
+                                                    <td>
+                                                        @if ($refill->status == 'pending')
+                                                            <span class="badge custom-badge-info">Pending</span>
+                                                        @endif
 
 
 
-                                                    @if ($refill->status == 'approved')
-                                                    <span class="badge custom-badge-success">Approved</span>
-                                                    @endif
+                                                        @if ($refill->status == 'approved')
+                                                            <span class="badge custom-badge-success">Approved</span>
+                                                        @endif
 
-                                                    @if ($refill->status == 'rejected')
-                                                    <span class="badge badge-danger px-3 py-1">Rejected</span>
-                                                    @endif
-                                                </td>
-                                            </tr>
+                                                        @if ($refill->status == 'rejected')
+                                                            <span class="badge badge-danger px-3 py-1">Rejected</span>
+                                                        @endif
+                                                    </td>
+                                                </tr>
                                             @endforeach
                                         </tbody>
                                     </table>
@@ -182,16 +188,16 @@
                                         </thead>
                                         <tbody>
                                             @foreach ($adAccounts as $adAccount)
-                                            <tr>
-                                                <td>{{ $adAccount->created_at->format('j F Y') }}</td>
-                                                <td>{{ $adAccount->client->name }}</td>
-                                                <td>
-                                                    <span>{{ $adAccount->ad_acc_name }}</span><br>
-                                                    <span class="font-sm mt-1">{{ $adAccount->ad_acc_id }}</span>
-                                                </td>
-                                                <td>{{ $adAccount->bm_id }}</td>
+                                                <tr>
+                                                    <td>{{ $adAccount->created_at->format('j F Y') }}</td>
+                                                    <td>{{ $adAccount->client->name }}</td>
+                                                    <td>
+                                                        <span>{{ $adAccount->ad_acc_name }}</span><br>
+                                                        <span class="font-sm mt-1">{{ $adAccount->ad_acc_id }}</span>
+                                                    </td>
+                                                    <td>{{ $adAccount->bm_id }}</td>
 
-                                            </tr>
+                                                </tr>
                                             @endforeach
                                         </tbody>
                                     </table>
@@ -222,15 +228,15 @@
                                         </thead>
                                         <tbody>
                                             @foreach ($refills as $refill)
-                                            @if($refill->status == 'pending')
-                                            <tr>
-                                                <td>{{ $refill->created_at->format('j F Y') }}</td>
-                                                <td>{{ $refill->adAccount->ad_acc_name }}</td>
-                                                <td>{{ $refill->amount_dollar }}</td>
-                                                <td>{{ $refill->payment_method }}</td>
+                                                @if ($refill->status == 'pending')
+                                                    <tr>
+                                                        <td>{{ $refill->created_at->format('j F Y') }}</td>
+                                                        <td>{{ $refill->adAccount->ad_acc_name }}</td>
+                                                        <td>{{ $refill->amount_dollar }}</td>
+                                                        <td>{{ $refill->payment_method }}</td>
 
-                                            </tr>
-                                            @endif
+                                                    </tr>
+                                                @endif
                                             @endforeach
                                         </tbody>
                                     </table>
@@ -257,6 +263,148 @@
         <!--**********************************
             Footer end
         ***********************************-->
+
+
+
+
+        <!-- Refill Modal -->
+        <div class="modal fade" id="refillModal" tabindex="-1" role="dialog" aria-labelledby="refillModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="refillModalLabel">New Refill</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('refill.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div>
+                                <label class="col-form-label">Client Name:</label>
+                                <select id="client-select" name="client_id" class="form-control rounded">
+                                    <option>Select</option>
+                                    @foreach ($customers as $customer)
+                                        <option value="{{ $customer->id }}">{{ $customer->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div>
+                                <label class="col-form-label">Ad Account Name:</label>
+                                <select id="ad-account-select" name="ad_account_id" class="form-control rounded">
+                                    <option>Select</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <label class="col-form-label">Dollar Rate:</label>
+                                <input id="dollar-rate-input" type="text" placeholder="Dollar Rate"
+                                    class="form-control rounded" readonly>
+                            </div>
+
+                            <div>
+                                <label class="col-form-label">Amount:</label><br>
+
+                                <div class="d-flex justify-content-between">
+                                    <div class="w-50 mr-2">
+                                        <input id="taka-input" type="text" name="amount_taka" placeholder="Taka"
+                                            class="form-control rounded">
+                                    </div>
+                                    <div class="w-50">
+                                        <input id="dollar-input" type="text" name="amount_dollar"
+                                            placeholder="Dollar" class="form-control rounded">
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div>
+                                <label class="col-form-label">Payment Method</label>
+                                <select id="payment_method" name="payment_method" class="form-control rounded">
+                                    <option>Select</option>
+                                    @foreach ($paymentMethods as $paymentMethod)
+                                        <option value="{{ $paymentMethod->value }}"
+                                            data-details="{{ $paymentMethod->details }}">{{ $paymentMethod->value }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div id="details" class="d-none">
+                                <p class="col-form-label font-bold">Payment Method Details: </p>
+                                <p id="payment_details"></p>
+                            </div>
+
+                            <div>
+                                <label class="col-form-label">Transaction Id:</label>
+                                <input type="text" name="transaction_id" placeholder="Transaction Id"
+                                    class="form-control rounded">
+                            </div>
+
+                            <div class="mt-2">
+                                <label class="col-form-label">Screenshot:</label>
+                                <div class="custom-file">
+                                    <input type="file" id="screenshot" name="screenshot"
+                                        class="custom-file-input">
+                                    <label class="custom-file-label">Choose file</label>
+                                </div>
+                            </div>
+
+                            <div class="d-flex justify-content-end mt-4">
+                                <input type="submit" name="submit" value="Refill" class="btn btn-primary">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Deposit Modal -->
+        <div class="modal fade" id="depositModal" tabindex="-1" role="dialog" aria-labelledby="depositModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="depositModalLabel">Add Deposit</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ route('deposit.store') }}" method="POST">
+                            @csrf
+                            <div>
+                                <label class="col-form-label">Vendor Name</label>
+                                <select name="name" class="form-control rounded">
+                                    <option>Select</option>
+                                    @foreach ($vendors as $vendor)
+                                        <option value="{{ $vendor->value }}">{{ $vendor->value }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+
+                            <div>
+                                <label class="col-form-label">Amount (USD):</label>
+                                <input type="number" step="0.01" name="amount_usd" placeholder="Amount (USD)"
+                                    class="form-control rounded" required>
+                            </div>
+
+                            <div>
+                                <label class="col-form-label">Rate (BDT):</label>
+                                <input type="number" step="0.01" name="rate_bdt" placeholder="Rate (BDT)"
+                                    class="form-control rounded" required>
+                            </div>
+
+                            <div class="d-flex justify-content-end mt-4">
+                                <input type="submit" name="submit" value="Create Deposit" class="btn btn-primary">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
     <!--**********************************
         Main wrapper end
@@ -266,6 +414,8 @@
         Scripts
     ***********************************-->
     @include('template.home.layouts.scripts')
+
+    @include('template.home.custom_scripts.refill_application_script')
 
     <script>
         var ctx = document.getElementById("pieChart");
